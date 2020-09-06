e277a34077f73648d4b9c1b1154770f77027019e
Bryan Swagerty <bryantee@gmail.com>
2020-09-06 10:53:12 -0700

    Ignore lib directory when running tests

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'jsx', 'js'],
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/lib/'],
 };
